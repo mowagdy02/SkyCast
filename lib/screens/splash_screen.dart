1506e4948fed1a41c005c2ee20b8085d1f50f932
Ziad Sameh <ziadsameh036@gmail.com>
2024-11-04 22:25:30 +0200

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -11,11 +11,11 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 3), () {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => Home()));
     });
-    super.initState();
   }
 
   @override
@@ -24,28 +24,50 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color(0xFF3E2D8F), Color(0xFF9D52AC)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight)),
+          gradient: LinearGradient(
+            colors: [Color(0xFF3E2D8F), Color(0xFF9D52AC)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Expanded(
-                child: Center(
-              child: Image.asset(
-                "assets/cloud_y.png",
-                height: 220,
-                width: 220,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/cloud_y.png",
+                    height: 200,
+                    width: 200,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "SkyCast",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 8.0,
+                          color: Colors.black38,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            )),
-            Expanded(
-                child: Center(
-              child: Text(
-                "SkyCast",
-                style: TextStyle(color: Colors.white, fontSize: 55),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
               ),
-            )),
+            ),
           ],
         ),
       ),
